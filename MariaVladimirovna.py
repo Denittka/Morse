6d0aa87ea8f9cf6d6f598001dc6f3a2ef5494ef9
Dan Belokon <dan.belokon@gmail.com>
2019-01-19 18:51:45 +0300

    Create MariaVladimirovna.py

---
+++ MariaVladimirovna.py
@@ -0,0 +1,12 @@
+def encode_to_morse(text):
+    MorseCode = {'A': '.-', 'B': '-...'}  # пока локальный словарь
+    # переводим в верхний регистр и разбиваем текст по символам пустого пространства
+    text = text.upper().split()
+    code_morse = []  # пустой список закодированных слов
+    for word in text:  # перебираем слова исходноо текста
+        string = ''  # каждое слово кодируем отдельно
+        for char in word:  # перебираем символы слова
+            if char in MorseCode:
+                string += MorseCode[char]  # кодируем
+        code_morse.append(string)  # добавляем закодированное слово в список
+    return code_morse  # или ' '.join(code_morse)
