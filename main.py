60bf25b5aba6494b9849d511f96798ce67cdcd70
Dan Belokon <dan.belokon@gmail.com>
2019-01-20 12:04:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,19 @@ def encode_to_morse(text):
     return code_morse  # или ' '.join(code_morse)
 
 
+def decode_from_morse(code):
+    new_MorseCode = {}
+    for codes, letters in MorseCode.items():
+        new_MorseCode[letters] = codes
+    # меняем местами ключи и значения(разворачиваем словарь)
+    code = code.split()  # создаем список закодированных символов
+    spisok = []  # пустой список для расшифрованных символов
+    for symb in code:  # перебираем зашифрованные символы
+        if symb in new_MorseCode:
+            spisok.append(new_MorseCode[symb]) # декодируем
+    return spisok
+
+
 def main():
     print("Введите, пожалуйста, функцию, которую необходимо выполнить (кодирование или декодирование):")
     func = input().lower()
